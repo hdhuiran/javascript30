9c9f879e4fad8224b6e6387d8963e6b6b5b5bda8
hdh <793079896@qq.com>
2025-09-09 00:27:30 +0800

    hold shift的index.js文件加了几行注释

--- Hold Shift and Check Checkboxes/script.js
+++ Hold Shift and Check Checkboxes/script.js
@@ -14,7 +14,9 @@ function handleCheck(e) {
     let isBetween = false;
     checkboxes.map((element) => {
       if (
-        (element === self || element === checkEle) && checkEle && checkEle !== self
+        (element === self || element === checkEle) &&
+        checkEle &&
+        checkEle !== self
       ) {
         isBetween = !isBetween;
       }
@@ -26,52 +28,51 @@ function handleCheck(e) {
   checkEle = self.checked ? self : null;
 }
 
-function extenCheck(e){
-    let self = this;
-    if(!e.target.checked){
-        return;
-    };
+function extenCheck(e) {
+  let self = this;
+  if (!e.target.checked) {
+    return;
+  }
 
-    let nowSelIndex = checkboxex_2.findIndex(element => element === self);
-    // console.log(nowSelIndex);
+  let nowSelIndex = checkboxex_2.findIndex((element) => element === self);
+  // console.log(nowSelIndex);
 
-    if(!e.shiftKey){
-        return;
-    }
+  if (!e.shiftKey) {
+    return;
+  }
 
-    const nearMaxCheckboxIndex = checkboxex_2.findIndex(
-        (element,index) =>{
-            return index !== nowSelIndex && index > nowSelIndex && element.checked === true;
-        }
-    )
-    // console.log(nearMaxCheckboxIndex);
+  const nearMaxCheckboxIndex = checkboxex_2.findIndex((element, index) => {
+    return (
+      index !== nowSelIndex && index > nowSelIndex && element.checked === true
+    );
+  });
+  console.log("nearMaxCheckboxIndex :" + nearMaxCheckboxIndex);
 
-    const nearMinCheckboxIndex = checkboxex_2.findIndex(
-        (element,index) =>{
-            return index !== nowSelIndex && index < nowSelIndex && element.checked === true;
-        }
-    )
+  const nearMinCheckboxIndex = checkboxex_2.findIndex((element, index) => {
+    return (
+      index !== nowSelIndex && index < nowSelIndex && element.checked === true
+    );
+  });
+  console.log("nearMinCheckboxIndex:" + nearMinCheckboxIndex);
 
-    if(nearMaxCheckboxIndex !== -1){
-        setCheckbox(nowSelIndex,nearMaxCheckboxIndex);
-    }
-    if(nearMinCheckboxIndex !== -1){
-        setCheckbox(nearMinCheckboxIndex,nowSelIndex);
-    }
-
+  if (nearMaxCheckboxIndex !== -1) {
+    setCheckbox(nowSelIndex, nearMaxCheckboxIndex);
+  }
+  if (nearMinCheckboxIndex !== -1) {
+    setCheckbox(nearMinCheckboxIndex, nowSelIndex);
+  }
 }
 
-function setCheckbox(initIndex,arrayLength){
-    for(let index = initIndex;index < arrayLength;index++){
-        checkboxex_2[index].checked = true;
-    }
+function setCheckbox(initIndex, arrayLength) {
+  for (let index = initIndex; index < arrayLength; index++) {
+    checkboxex_2[index].checked = true;
+  }
 }
-
 
 checkboxes.map((element) => {
   element.addEventListener("click", handleCheck);
 });
 
-checkboxex_2.map((element) =>{
-    element.addEventListener("click",extenCheck);
-})
+checkboxex_2.map((element) => {
+  element.addEventListener("click", extenCheck);
+});
